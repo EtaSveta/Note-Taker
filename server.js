3ee980a109aa580dc3449b6a946ff1aded0c3839
Svetlana Nesterova <sveta59n@yahoo.com>
2022-10-14 10:59:27 -0400

    add heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+
+const app = express();
+
+const { db } = require('./db/db');
+
+
+
+app.get('/api/db', (req, res) => {
+  res.json(db);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
