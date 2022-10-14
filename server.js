a8f207844b464e2e12bd8ec5dc85f2302f32a4c2
Svetlana Nesterova <sveta59n@yahoo.com>
2022-10-14 16:57:36 -0400

    adding a POST request

--- server.js
+++ server.js
@@ -5,12 +5,28 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const { db } = require('./db/db.json');
+const { notes } = require('./db/db.json');
 
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+//parse incoming string or array data
+app.use(express.urlencoded({ extended:true }));
+//parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
 
+// // Use apiRoutes
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 app.get('/api/db', (req, res) => {
-  res.json(db);
+  res.json(notes);
+});
+
+app.post('/api/db', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(PORT, () => {
